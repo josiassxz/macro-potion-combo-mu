3bb3f4c01ffda87fd2b9aae08275b28a798f99eb
josias <josiassxz@hotmail.com>
2024-06-11 00:09:11 -0300

    ajustado ativar janela

--- macro-jitbt-c#-irpara ds.cs
+++ macro-jitbt-c#-irpara ds.cs
@@ -13,6 +13,13 @@ public class Program
     [DllImport("user32.dll", SetLastError = true)]
     static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
 
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool SetForegroundWindow(IntPtr hWnd);
+
     private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
     private const uint MOUSEEVENTF_LEFTUP = 0x04;
 
@@ -26,10 +33,9 @@ public class Program
             DateTime now = DateTime.Now;
             int hour = now.Hour;
             int minute = now.Minute;
-            int segundos = now.Second;
 
             // Verifica se a hora e o minuto correspondem aos horários específicos
-            if ((hour == 00 && minute == 27) ||
+            if ((hour == 0 && minute == 27) ||
                 (hour == 2 && minute == 27) ||
                 (hour == 4 && minute == 27) ||
                 (hour == 6 && minute == 27) ||
@@ -39,10 +45,9 @@ public class Program
                 (hour == 14 && minute == 27) ||
                 (hour == 16 && minute == 27) ||
                 (hour == 18 && minute == 27) ||
-                (hour == 22 && minute == 27) ||
-                (hour == 20 && minute == 27))
+                (hour == 20 && minute == 27) ||
+                (hour == 22 && minute == 27))
             {
-                
                 ExecuteCode();
             }
 
@@ -53,47 +58,49 @@ public class Program
 
     private static void ExecuteCode()
     {
+        // Ativa a janela SuperMU se não estiver ativa
+        IntPtr hWnd = FindWindow("SuperMU", null);
+        if (hWnd != IntPtr.Zero)
+        {
+            SetForegroundWindow(hWnd);
+        }
 
-          Thread.Sleep(3000);
+        Thread.Sleep(3000);
 
-
-        //digita enter
-         keybd_event(0x0D, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+        // Digita enter
+        keybd_event(0x0D, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
         Thread.Sleep(200); // Pequeno delay para garantir que a tecla é pressionada
         keybd_event(0x0D, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
 
         Thread.Sleep(2000);
-      
 
         // Digita "/"
         keybd_event(0xBF, 0, 0, (UIntPtr)0); // Código da tecla "/"
         Thread.Sleep(200); // Pequeno delay para garantir que a tecla é pressionada
         keybd_event(0xBF, 0, KEYEVENTF_KEYUP, (UIntPtr)0); // Solta a tecla "/"
 
-         Thread.Sleep(2000);
+        Thread.Sleep(2000);
 
         // Digita "c"
         keybd_event(0x43, 0, 0, (UIntPtr)0); // Código da tecla "c"
         Thread.Sleep(200); // Pequeno delay para garantir que a tecla é pressionada
         keybd_event(0x43, 0, KEYEVENTF_KEYUP, (UIntPtr)0); // Solta a tecla "c"
 
-         Thread.Sleep(2000);
-        
-        
+        Thread.Sleep(2000);
+
         // Código da tecla "m"
-        keybd_event(0x4D, 0, 0, (UIntPtr)0); 
+        keybd_event(0x4D, 0, 0, (UIntPtr)0);
         Thread.Sleep(200); // Pequeno delay para garantir que a tecla é pressionada
         keybd_event(0x4D, 0, KEYEVENTF_KEYUP, (UIntPtr)0); // Solta a tecla "m"
 
-         Thread.Sleep(2000);
+        Thread.Sleep(2000);
 
-        //digita enter
-         keybd_event(0x0D, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+        // Digita enter
+        keybd_event(0x0D, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
         Thread.Sleep(200); // Pequeno delay para garantir que a tecla é pressionada
         keybd_event(0x0D, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
 
-
-        Thread.Sleep(8000);
+        Thread.Sleep(5000);
 
         // Move até o NPC
         SetCursorPos(739, 209); // ALTERAR AQUI
@@ -103,7 +110,7 @@ public class Program
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         Thread.Sleep(50);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-        Thread.Sleep(8000);
+        Thread.Sleep(5000);
 
         // Move para a posição ds 6
         SetCursorPos(1359, 812); // ALTERAR AQUI
@@ -116,7 +123,7 @@ public class Program
         Thread.Sleep(10000);
 
         // Move até a posição do spot
-        SetCursorPos(1473, 627);  // ALTERAR AQUI
+        SetCursorPos(1473, 627); // ALTERAR AQUI
         Thread.Sleep(1000);
 
         // Clica na posição do spot
@@ -126,8 +133,8 @@ public class Program
 
         Thread.Sleep(3000);
 
-         // Move até a posição do spot
-        SetCursorPos(1476, 669);  // ALTERAR AQUI
+        // Move até a posição do spot
+        SetCursorPos(1476, 669); // ALTERAR AQUI
         Thread.Sleep(1000);
 
         // Clica na posição do spot
@@ -137,13 +144,20 @@ public class Program
 
         Thread.Sleep(3000);
 
-       // Pressiona a tecla Home
+        // Pressiona a tecla Home
         keybd_event(0x24, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
         Thread.Sleep(50);
         keybd_event(0x24, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
 
-     
         Thread.Sleep(25 * 60 * 1000); // Espera 30 minutos ds acabar
+
+
+        // Ativa a janela SuperMU se não estiver ativa
+        IntPtr hWnd = FindWindow("SuperMU", null);
+        if (hWnd != IntPtr.Zero)
+        {
+            SetForegroundWindow(hWnd);
+        }
 
         Thread.Sleep(3000);
 
@@ -152,63 +166,55 @@ public class Program
         Thread.Sleep(50);
         keybd_event(0x0D, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
 
-
         Thread.Sleep(5000);
 
-            PressKeySequenceSpot("/kanturuvip23");
+        PressKeySequenceSpot("/kanturuvip23");
 
         Thread.Sleep(3000);
 
-        //mover cursor para clicar spot
-        SetCursorPos(445, 969); 
+        // Mover cursor para clicar spot
+        SetCursorPos(445, 969);
         Thread.Sleep(1000);
 
-        //clicar no spot1
+        // Clicar no spot1
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         Thread.Sleep(50);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
         Thread.Sleep(3000);
 
-        //clicar no spot2
+        // Clicar no spot2
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         Thread.Sleep(50);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
         Thread.Sleep(1000);
 
-        //mover cursor para clicar spot3
-        SetCursorPos(1472, 781); 
+        // Mover cursor para clicar spot3
+        SetCursorPos(1472, 781);
         Thread.Sleep(1000);
 
-        //clicar no spot3
+        // Clicar no spot3
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         Thread.Sleep(50);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 
         Thread.Sleep(3000);
-
-
 
         // Pressiona a tecla Home
         keybd_event(0x24, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
         Thread.Sleep(50);
         keybd_event(0x24, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
- 
-
-
-
- 
     }
 
     private static void PressKeySequenceSpot(string text)
     {
         Thread.Sleep(5000);
 
-         // Pressiona Enter
+        // Pressiona Enter
         keybd_event(0x0D, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
         Thread.Sleep(100);
         keybd_event(0x0D, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
 
-         Thread.Sleep(2000);
+        Thread.Sleep(2000);
 
         foreach (char c in text)
         {
@@ -219,15 +225,13 @@ public class Program
             Thread.Sleep(100);
         }
 
-         Thread.Sleep(2000);
+        Thread.Sleep(2000);
 
         // Pressiona Enter
         keybd_event(0x0D, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
         Thread.Sleep(100);
         keybd_event(0x0D, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
     }
-
-
 
     private static byte VkKeyScanSpot(char ch)
     {
